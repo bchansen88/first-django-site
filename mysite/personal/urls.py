3ec3286692842599d1d7f727c6184491c5e97c2a
bchansen88 <b.c.hansen88@gmail.com>
2018-03-25 00:18:47 -0400

    Finished Jinja Virables tutorial

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	#homepage
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^contact/', views.contact, name='contact'),
 ]
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
 	
 	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['If you\'d like to contact me at:',
+															'notmyrealemail@gmail.com']})
